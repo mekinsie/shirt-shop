bb62f96980271ffc61080b5f1ce07396cf4afc78
Dani Renner <danijrenner@gmail.com>
2021-04-22 15:07:48 -0700

    add ability to see specific shirt details
    Co-authored-by: mekinsie <mekinsie.aja@gmail.com>

---
+++ src/components/ShirtDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+// import Shirt from "./Shirt";
+
+function ShirtDetail(props){
+  const{shirt, onClickingEdit }=props;
+
+  return (
+    <React.Fragment>
+      <h3>{shirt.name}</h3>
+      <h3>Description: {shirt.description}</h3>
+      <h3>Quantity Available: {shirt.quantity}</h3>
+      <h3>Price: {shirt.price}</h3>
+      <button onClick={ onClickingEdit }>Update Shirt</button>
+    </React.Fragment>
+  );
+}
+
+ShirtDetail.propTypes = {
+  shirt: PropTypes.object,
+  onClickingEdit: PropTypes.func
+};
+
+export default ShirtDetail;
