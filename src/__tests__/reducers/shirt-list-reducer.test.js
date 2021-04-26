8e75226c2aae1ae7208081fbab5342426d92b2dc
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-04-26 13:44:35 -0700

    Add reducer cases to add and delete shirts.
    Co-authored-by: Morgan Bradford <morganjbradford95+github@gmail.com>

--- src/__tests__/reducers/shirt-list-reducer.test.js
+++ src/__tests__/reducers/shirt-list-reducer.test.js
@@ -2,14 +2,71 @@ import shirtListReducer from '../../reducers/shirt-list-reducer';
 
 describe('shirtListReducer', () => {
 
-  // let action;
-  // const currentState = {
-    
-  // }
+  let action;
+  const shopData = {
+    name: "Some Brand",
+    description: "A floral t-shirt",
+    price: "$50",
+    quantity: "20",
+    id: 1
+  }
+
+  const currentState = {
+    1: {
+      name: 'cool shirt',
+      description: 'a shirt with circles',
+      price: '32.99',
+      quantity: '4',
+      id: 1
+    },
+    2: {
+      name: "Some Brand",
+      description: "A floral t-shirt",
+      price: "$50",
+      quantity: "20",
+      id: 2
+    }
+  }
 
   test('Should return default state if no action type is recognized', () => {
     expect(shirtListReducer({}, { type: null })).toEqual({});
   });
 
+  test('should successfully add a new shop data to masterShopList', () => {
+    const { name, description, price, quantity, id } = shopData;
+    action = {
+      type: "ADD_ITEM",
+      name: name,
+      description: description,
+      price: price,
+      quantity: quantity,
+      id: id
+    };
 
-})
+    expect(shirtListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        description: description,
+        price: price,
+        quantity: quantity,
+        id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a shirt', () => {
+    action = {
+      type: 'DELETE_ITEM',
+      id: 1
+    };
+    expect(shirtListReducer(currentState, action)).toEqual({
+      2: {
+        name: "Some Brand",
+        description: "A floral t-shirt",
+        price: "$50",
+        quantity: "20",
+        id: 2
+      }
+    });
+  });
+});
--- src/reducers/shirt-list-reducer.js
+++ src/reducers/shirt-list-reducer.js
@@ -1,3 +1,21 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, description, price, quantity, id } = action;
+  switch(action.type) {
+    case 'ADD_ITEM':
+      return Object.assign({}, state, {
+        [id] : {
+          name: name,
+          description: description,
+          price: price,
+          quantity: quantity,
+          id: id
+        }
+      });
+    case 'DELETE_ITEM':
+      let newState = { ...state };
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
 };
